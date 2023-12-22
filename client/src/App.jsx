a1371b4bfdf311ffaf66120ff7abfadc2d1e7521
Darius <dariuskumza@gmail.com>
2023-12-22 13:22:37 +0200

    refactor with useContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,98 +5,18 @@ import "./App.css";
 import { Users } from "./components/Users";
 import { Input } from "./components/Input";
 import { DeleteModal } from "./components/DeleteModal";
-
-const USERS_URL = "http://localhost:3000/api/users";
+import { UsersProvider } from "./components/UsersContext";
 
 function App() {
-   const [users, setUsers] = useState(null);
-   const [editing, setEditing] = useState(false);
-   const [editingUser, setEditingUser] = useState(null);
-   const [del, setDel] = useState(false);
-   const [toDel, setToDel] = useState(null);
-   // show name of editing user
-   const [originalName, setOriginalName] = useState("");
-
-   useEffect(() => {
-      axios
-         .get(USERS_URL)
-         .then((res) => {
-            setUsers(res.data);
-         })
-         .catch((error) => {
-            console.warn("Error:", error);
-         });
-   }, []);
-
-   const handleEditUser = (user) => {
-      setEditingUser(user);
-      setOriginalName(user.name); // create static name to show on edit screen
-      setEditing(true);
-   };
-
-   const handleDelete = (toDel) => {
-      axios
-         .delete(`${USERS_URL}/${toDel}`)
-         .then((res) => {
-            setUsers(res.data);
-         })
-         .catch((error) => {
-            console.warn("Error:", error);
-         });
-   };
-
-   const inputProps = {
-      setUsers,
-      editing,
-      setEditing,
-      editingUser,
-      setEditingUser,
-      originalName,
-   };
-
-   const userProps = {
-      users,
-      handleEditUser,
-      setDel,
-      setToDel,
-   };
-
-   const delModalProps = {
-      setDel,
-      toDel,
-      setToDel,
-      handleDelete,
-   };
-
    return (
-      <div className="container mx-auto md:w-4/6 min-h-screen min-w-full p-12 relative flex flex-col items-center">
-         {del ? <DeleteModal {...delModalProps} /> : null}
-         <Input {...inputProps} />
-         <h1 className="text-center mb-4 font-semibold text-2xl">
-            Users From Server
-         </h1>
-         <Users {...userProps} />
-      </div>
+      <UsersProvider>
+         <div className="container mx-auto md:w-4/6 min-h-screen min-w-full p-12 relative flex flex-col items-center">
+            {/* {del ? <DeleteModal {...delModalProps} /> : null} */}
+            <Input />
+            <Users />
+         </div>
+      </UsersProvider>
    );
 }
 
 export default App;
-
-// const USERS_TOWNS = "http://localhost:3000/api/users/town";
-{
-   /* <UsersTowns
-            handleTowns={handleTowns}
-            showTowns={showTowns}
-            towns={towns}
-         /> */
-}
-
-// shows users towns on click
-// const [showTowns, setShowTowns] = useState(false);
-// const [towns, setTowns] = useState(null);
-// const handleTowns = () => {
-//    setShowTowns((prevState) => !prevState);
-//    axios.get(USERS_TOWNS).then((res) => {
-//       setTowns(res.data);
-//    });
-// };
---
+++ client/src/components/UsersContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useState } from "react";
+
+export const UsersContext = createContext();
+
+const USERS_URL = "http://localhost:3000/api/users";
+
+export const UsersProvider = ({ children }) => {
+   const [users, setUsers] = useState(null); // users from DB
+   const [editing, setEditing] = useState(false); // state for editing
+   const [editingUser, setEditingUser] = useState(null); // state for editing user
+   const [del, setDel] = useState(false); // state to show modal to delete
+   const [toDel, setToDel] = useState(null); // state for delete user
+   const [originalName, setOriginalName] = useState(""); // show name of editing user as static value
+
+   useEffect(() => {
+      axios
+         .get(USERS_URL)
+         .then((res) => {
+            setUsers(res.data);
+         })
+         .catch((error) => {
+            console.warn("Error:", error);
+         });
+   }, []);
+
+   const handleEditUser = (user) => {
+      setEditingUser(user);
+      setOriginalName(user.name); // create static name to show on edit screen
+      setEditing(true);
+   };
+
+   const handleDelete = (toDel) => {
+      axios
+         .delete(`${USERS_URL}/${toDel}`)
+         .then((res) => {
+            setUsers(res.data);
+         })
+         .catch((error) => {
+            console.warn("Error:", error);
+         });
+   };
+
+   return (
+      <UsersContext.Provider>
+         value={" "}
+         {{
+            users,
+            setUsers,
+            editing,
+            setEditing,
+            editingUser,
+            setEditingUser,
+            del,
+            setDel,
+            toDel,
+            setToDel,
+            originalName,
+            setOriginalName,
+         }}
+         {children}
+      </UsersContext.Provider>
+   );
+};
